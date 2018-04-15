42aefa76632eb5a68dc7b7d91b086d0fe96719ea
harry <harry74323@gmail.com>
2018-04-15 10:23:22 +1000

    tweaking layout

--- src/app/has-angular-material/has-angular-material.component.ts
+++ src/app/has-angular-material/has-angular-material.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-has-angular-material',
   templateUrl: './has-angular-material.component.html',
   styleUrls: ['./has-angular-material.component.css']
 })
 export class HasAngularMaterialComponent implements OnInit {
-
-  constructor() { }
+  complexForm: FormGroup;
+  constructor(fb: FormBuilder) {
+    this.complexForm = fb.group({
+      'firstName' : [null, Validators.required]
+    })
+  }
 
   ngOnInit() {
   }
 
+  onClick(e) {
+    e.preventDefault();
+    console.log(this.complexForm);
+  }
+
+  submitForm(value: any) {
+    console.log(value);
+  }
 }
