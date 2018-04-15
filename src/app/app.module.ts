640e588963a11b64b478f037875d70ee5128bb88
harry <harry74323@gmail.com>
2018-04-15 10:04:44 +1000

    no material first field working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AngularMaterialLibrary } from './angular-material-library/angular-material-library.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NoAngularMaterialComponent } from './no-angular-material/no-angular-material.component';
@@ -16,7 +17,7 @@ import { HasAngularMaterialComponent } from './has-angular-material/has-angular-
     HasAngularMaterialComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, AngularMaterialLibrary
+    BrowserModule, BrowserAnimationsModule, AngularMaterialLibrary, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/no-angular-material/no-angular-material.component.ts
+++ src/app/no-angular-material/no-angular-material.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-no-angular-material',
   templateUrl: './no-angular-material.component.html',
   styleUrls: ['./no-angular-material.component.css']
 })
 export class NoAngularMaterialComponent implements OnInit {
-
-  constructor() { }
+  complexForm: FormGroup;
+  constructor(fb: FormBuilder) {
+    this.complexForm = fb.group({
+      'firstName' : [null, Validators.required]
+    })
+  }
 
   ngOnInit() {
   }
 
+  onClick(e) {
+    e.preventDefault();
+    console.log(this.complexForm);
+  }
+
+  submitForm(value: any) {
+    console.log(value);
+  }
 }
